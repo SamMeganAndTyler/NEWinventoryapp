a3ed67ef009de3cbbd77881f67956e7180d7e039
Tyler <128461465+Tylerhayman@users.noreply.github.com>
2023-05-24 12:30:28 +0000

    Modified the database structure

--- server/models/item.js
+++ server/models/item.js
@@ -1,11 +1,5 @@
-const { Sequelize, DataTypes } = window.Sequelize;
-
-const sequelize = new Sequelize('database', 'name', 'description', 'price', 'category', 'image', {
-  host: 'localhost',
-  dialect: 'mysql',
- 
-});
-
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database');
 
 const Item = sequelize.define('Item', {
   name: {
@@ -30,27 +24,4 @@ const Item = sequelize.define('Item', {
   },
 });
 
-// Create a new item
-const newItem = await Item.create({
-  name: 'Sample Item',
-  description: 'This is a sample item',
-  price: 10.99,
-  category: 'Sample Category',
-  image: 'sample.jpg',
-});
-
-// Retrieve all items
-const allItems = await Item.findAll();
-
-// Example: Updating an item
-const itemToUpdate = await Item.findByPk(itemId);
-if (itemToUpdate) {
-  itemToUpdate.name = 'Updated Item Name';
-  await itemToUpdate.save();
-}
-
-// Deleting an item
-const itemToDelete = await Item.findByPk(itemId);
-if (itemToDelete) {
-  await itemToDelete.destroy();
-}
+module.exports = Item;
